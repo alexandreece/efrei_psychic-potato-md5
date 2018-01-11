090d9fbdce1836e6e712c237996a75b54efcccc2
Alexandre <alexandre.suhas@edu.ecetech.fr>
2018-01-11 17:39:08 +0100

    Clean up

--- src/Initializer.kt
+++ src/Initializer.kt
@@ -1,11 +1,11 @@
 class Initializer {
 
-    fun appendMessage(msg : String): IntArray{
+    fun appendMessage(msg: String): IntArray {
         //Size appended to the message in bytes
         val appSize = 64 - ((msg.length + 9) % 64) + 9
         var result = IntArray(msg.length + appSize)
         //Transform string into int array
-        for(i in msg.indices){
+        for (i in msg.indices) {
             result[i] = msg[i].toInt()
         }
         //append padding
@@ -16,12 +16,12 @@ class Initializer {
     }
 
     //Transform the size of the message(msgSize here) in to bytes that are appended to the message
-    private fun appendSize(msg : IntArray, msgLen: Int): IntArray {
+    private fun appendSize(msg: IntArray, msgLen: Int): IntArray {
         val result = msg
         var rem = msgLen * 8
 
-        for(i in 0..7) {
-            if(rem > 0){
+        for (i in 0..7) {
+            if (rem > 0) {
                 result[msg.size - (8 - i)] = rem % 256
                 rem /= 256
             } else break
@@ -30,14 +30,14 @@ class Initializer {
     }
 
 
-    fun bytesTo32bitsArray(appendMsg : IntArray): LongArray {
+    fun bytesTo32bitsArray(appendMsg: IntArray): LongArray {
         val result = LongArray(appendMsg.size / 4)
-        var sum : Long = 0
+        var sum: Long = 0
         var pow = 1
-        for(i in appendMsg.indices) {
+        for (i in appendMsg.indices) {
             sum += appendMsg[i].toLong() * pow
             pow *= 256
-            if(i % 4 == 3){
+            if (i % 4 == 3) {
                 result[i / 4] = sum
                 sum = 0
                 pow = 1
--- src/Main.kt
+++ src/Main.kt
@@ -1,49 +1,51 @@
-fun main(args : Array<String>){
-   //Message
-   val init = Initializer()
-   val msg = "Hello World!"
-   println("Message :")
-   println(msg)
-   // Array M composed of 32bits words
-   val tableMsg = init.bytesTo32bitsArray(init.appendMessage(msg))
-   println("Table M from the message")
-   displayHexArray(toBytesArray(tableMsg))
+fun main(args: Array<String>) {
+    //Message
+    val init = Initializer()
+    val msg = "Hello World!"
+    println("Message :")
+    println(msg)
+    // Array M composed of 32bits words
+    val tableMsg = init.bytesTo32bitsArray(init.appendMessage(msg))
+    println("Table M from the message")
+    displayHexArray(toBytesArray(tableMsg))
 
-   val rds = Rounds()
-   val hash = rds.encrypt(tableMsg)
-   println("Hash MD5 : ")
-   displayBytesArray(hash)
+    val rds = Rounds()
+    val hash = rds.encrypt(tableMsg)
+    println("Hash MD5 : ")
+    displayBytesArray(hash)
 }
 
 //Go from 32bits word array to 8bits word array0
-fun toBytesArray(arr : LongArray): IntArray{
-   val result = IntArray(arr.size * 4)
-   for(i in arr.indices){
-      var num = arr[i]
-      for(j in 0..3){
-         result[i * 4 + j] = (num % 256).toInt()
-         num /= 256
-      }
+fun toBytesArray(arr: LongArray): IntArray {
+    val result = IntArray(arr.size * 4)
+    for (i in arr.indices) {
+        var num = arr[i]
+        for (j in 0..3) {
+            result[i * 4 + j] = (num % 256).toInt()
+            num /= 256
+        }
 
-   }
-   return result
-}
-fun displayBytesArray(arr : IntArray){
-   for(i in arr.indices){
-      displayHex(arr[i])
-   }
-}
-fun displayHexArray(M : IntArray){
-   for(i in M.indices){
-      displayHex(M[(i / 4) * 4 + (3 - (i % 4))])
-      if(i % 4 == 3) println()
-   }
-
+    }
+    return result
 }
 
-fun displayHex(num : Int){
-   val hex = charArrayOf('0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F')
-   print(hex[num / 16])
-   print(hex[num % 16])
-   print(" ")
+fun displayBytesArray(arr: IntArray) {
+    for (i in arr.indices) {
+        displayHex(arr[i])
+    }
+}
+
+fun displayHexArray(M: IntArray) {
+    for (i in M.indices) {
+        displayHex(M[(i / 4) * 4 + (3 - (i % 4))])
+        if (i % 4 == 3) println()
+    }
+
+}
+
+fun displayHex(num: Int) {
+    val hex = charArrayOf('0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F')
+    print(hex[num / 16])
+    print(hex[num % 16])
+    print(" ")
 }
