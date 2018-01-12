7ca9a24d553dd0f35dbe21f34a49c2a75b306a9c
Alexandre <alexandre.suhas@edu.ecetech.fr>
2018-01-12 12:11:27 +0100

    Part2 jar

--- src/Main.kt
+++ src/Main.kt
@@ -3,7 +3,7 @@ import java.io.File
 
 fun main(args: Array<String>) {
     val passwords = generatePassword(100);
-    File("../passwordsPart2.txt").printWriter().use { out ->
+    File("./passwordsPart2.txt").printWriter().use { out ->
         passwords.forEach {
             val hash = MD5(it.value)
             out.println("${it.key} -> ${it.value} -> MD5: ${hash}")
