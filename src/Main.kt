5137503a2215cf36c8e6b28d62550ad49f11e6de
Alexandre <alexandre.suhas@edu.ecetech.fr>
2018-01-12 12:00:58 +0100

    Part 2 ok

--- src/Main.kt
+++ src/Main.kt
@@ -1,3 +1,12 @@
-fun main(args: Array<String>) {
+import pwdgenerator.generatePassword
+import java.io.File
 
+fun main(args: Array<String>) {
+    val passwords = generatePassword(100);
+    File("../passwordsPart2.txt").printWriter().use { out ->
+        passwords.forEach {
+            val hash = MD5(it.value)
+            out.println("${it.key} -> ${it.value} -> MD5: ${hash}")
+        }
+    }
 }
