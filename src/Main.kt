90e6625cc096460db30eabbb17d62c2eb814e3e7
Hyponeros <arthur.schickel@hotmail.fr>
2018-01-19 12:10:24 +0100

    MD5 corrected

--- src/Main.kt
+++ src/Main.kt
@@ -2,7 +2,8 @@ import pwdgenerator.generatePassword
 import java.io.File
 
 fun main(args: Array<String>) {
-    val passwords = generatePassword(100);
+
+    val passwords = generatePassword(100)
     File("./passwordsPart2.txt").printWriter().use { out ->
         passwords.forEach {
             val hash = MD5(it.value)
