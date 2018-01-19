ec1b3c5d91a4f09f255d79dc1d57d0f2f519ba29
Hyponeros <arthur.schickel@hotmail.fr>
2018-01-19 12:11:51 +0100

    MD5 corrected

--- src/Main.kt
+++ src/Main.kt
@@ -2,7 +2,6 @@ import pwdgenerator.generatePassword
 import java.io.File
 
 fun main(args: Array<String>) {
-
     val passwords = generatePassword(100)
     File("./passwordsPart2.txt").printWriter().use { out ->
         passwords.forEach {
